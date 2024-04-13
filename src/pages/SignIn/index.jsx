01f2872636a4e9819a371ca897e81357fc5b2408
henrico <henrico.an@gmail.com>
2024-04-13 19:51:47 -0300

    feat: adding login event

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,33 +1,52 @@
-import { useState } from "react";
+import ReactGA from 'react-ga4'
+import { useState } from 'react'
 
-import { Container, ContentBox } from "./styles";
+import { Container, ContentBox } from './styles'
 
-import { useAuth } from "../../hooks/auth";
+import { useAuth } from '../../hooks/auth'
 
-import { Logo } from "../../components/Logo";
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
-import { TextButton } from "../../components/TextButton";
+import { Logo } from '../../components/Logo'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { TextButton } from '../../components/TextButton'
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const { signIn } = useAuth();
+  const { signIn } = useAuth()
 
   function handleSignIn() {
-    signIn({ email, password });
+    signIn({ email, password })
+    ReactGA.event('login')
   }
 
   return (
     <Container>
-      <Logo isbiggersize/>
+      <Logo isbiggersize />
 
       <ContentBox>
         <h2>Faça Login</h2>
-        <Input id="email" type="email" border labelContent="Email" lesspace placeholder="Exemplo: exemplo@exemplo.com.br" onChange={e => setEmail(e.target.value)}/>
-        <Input id="password" border type="password" labelContent="Senha" lesspace placeholder="No mínimo 6 caracteres" min="6" onChange={e => setPassword(e.target.value)}/>
-        <Button title="Entrar" isbiggerfont onClick={handleSignIn}/>
+        <Input
+          id="email"
+          type="email"
+          border
+          labelContent="Email"
+          lesspace
+          placeholder="Exemplo: exemplo@exemplo.com.br"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <Input
+          id="password"
+          border
+          type="password"
+          labelContent="Senha"
+          lesspace
+          placeholder="No mínimo 6 caracteres"
+          min="6"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Button title="Entrar" isbiggerfont onClick={handleSignIn} />
         <TextButton to="/register" title="Criar conta" />
       </ContentBox>
     </Container>
