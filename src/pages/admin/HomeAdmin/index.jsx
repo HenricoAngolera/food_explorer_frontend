f815706fe9d089a223ccc64b1c977656a03a3047
henrico <henrico.an@gmail.com>
2024-04-13 20:12:23 -0300

    feat: adding search event

--- src/pages/admin/HomeAdmin/index.jsx
+++ src/pages/admin/HomeAdmin/index.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ReactGA } from 'react-ga4'
 
 import { Header } from '../../../components/Header'
 import { Footer } from '../../../components/Footer'
@@ -8,25 +9,36 @@ import { CardData } from '../../../components/CardData'
 
 import macarrons from '../../../assets/images/marrons.png'
 
-import { api } from "../../../services/api"
+import { api } from '../../../services/api'
 
-import { Container, Content, Scroll, HomeMessage, Text, Sections, Cards } from './styles'
+import {
+  Container,
+  Content,
+  Scroll,
+  HomeMessage,
+  Text,
+  Sections,
+  Cards
+} from './styles'
 
 export function HomeAdmin() {
-  const [dishes, setDishes] = useState([]);
-  const [mainCourses, setMainCourses] = useState([]);
-  const [drinks, setDrinks] = useState([]);
+  const [dishes, setDishes] = useState([])
+  const [mainCourses, setMainCourses] = useState([])
+  const [drinks, setDrinks] = useState([])
 
-  const [search, setSearch] = useState("");
-  
-  const navigate = useNavigate();
+  const [search, setSearch] = useState('')
+
+  const navigate = useNavigate()
 
   async function fetchDishes() {
-    const { data } = await api.get(`/dishes?name=${search}`);
+    const { data } = await api.get(`/dishes?name=${search}`)
 
-    setDishes(data.filter((dish) => dish.category === "dish"))
-    setMainCourses(data.filter((mainCourse) => mainCourse.category === "main course"))
-    setDrinks(data.filter((drink) => drink.category === "drink"))
+    setDishes(data.filter(dish => dish.category === 'dish'))
+    setMainCourses(
+      data.filter(mainCourse => mainCourse.category === 'main course')
+    )
+    setDrinks(data.filter(drink => drink.category === 'drink'))
+    ReactGA.event('search', search)
   }
 
   fetchDishes()
@@ -55,23 +67,53 @@ export function HomeAdmin() {
           <Sections>
             <Section title="Refeições">
               <Cards>
-                { dishes && dishes.map((dish) => (
-                  <CardData key={String(dish.id)} image={`${api.defaults.baseURL}/files/${dish.image}`} name={dish.name} description={dish.description} price={dish.price} isbiggerfont onClick={() => handleDetails(dish.id)} dishId={dish.id} />
-                ))}
+                {dishes &&
+                  dishes.map(dish => (
+                    <CardData
+                      key={String(dish.id)}
+                      image={`${api.defaults.baseURL}/files/${dish.image}`}
+                      name={dish.name}
+                      description={dish.description}
+                      price={dish.price}
+                      isbiggerfont
+                      onClick={() => handleDetails(dish.id)}
+                      dishId={dish.id}
+                    />
+                  ))}
               </Cards>
             </Section>
             <Section title="Pratos principais">
               <Cards>
-                { mainCourses && mainCourses.map((mainCourse) => (
-                  <CardData key={String(mainCourse.id)} image={`${api.defaults.baseURL}/files/${mainCourse.image}`} name={mainCourse.name} description={mainCourse.description} price={mainCourse.price} isbiggerfont onClick={() => handleDetails(mainCourse.id)} dishId={mainCourse.id} />
-                ))}
+                {mainCourses &&
+                  mainCourses.map(mainCourse => (
+                    <CardData
+                      key={String(mainCourse.id)}
+                      image={`${api.defaults.baseURL}/files/${mainCourse.image}`}
+                      name={mainCourse.name}
+                      description={mainCourse.description}
+                      price={mainCourse.price}
+                      isbiggerfont
+                      onClick={() => handleDetails(mainCourse.id)}
+                      dishId={mainCourse.id}
+                    />
+                  ))}
               </Cards>
             </Section>
             <Section title="Bebidas">
               <Cards>
-                { drinks && drinks.map((drink) => (
-                  <CardData key={String(drink.id)} image={`${api.defaults.baseURL}/files/${drink.image}`} name={drink.name} description={drink.description} price={drink.price} isbiggerfont onClick={() => handleDetails(drink.id)} dishId={drink.id} />
-                ))}
+                {drinks &&
+                  drinks.map(drink => (
+                    <CardData
+                      key={String(drink.id)}
+                      image={`${api.defaults.baseURL}/files/${drink.image}`}
+                      name={drink.name}
+                      description={drink.description}
+                      price={drink.price}
+                      isbiggerfont
+                      onClick={() => handleDetails(drink.id)}
+                      dishId={drink.id}
+                    />
+                  ))}
               </Cards>
             </Section>
           </Sections>
